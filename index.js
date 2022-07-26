a620dca1e3dd627e24eecff712af60b56743c942
Saira Fatima <saira030854@gmail.com>
2022-07-26 12:48:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View} from "react-native";
+import { View } from "react-native";
 import { ViewPropTypes } from 'deprecated-react-native-prop-types';
 import PropTypes from "prop-types";
 import xmldom from "xmldom";
